2af161e5b7ec52df40a77eb90a1ffa880a1acd50
Benedikt Linke <benedikt.linke@4soft.de>
2020-11-03 12:45:57 +0100

    prepare HasDescription for all vec versions

--- vec120/src/main/java/com/foursoft/vecmodel/vec120/common/HasDescription.java
+++ vec120/src/main/java/com/foursoft/vecmodel/vec120/common/HasDescription.java
@@ -10,10 +10,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,46 +25,42 @@
  */
 package com.foursoft.vecmodel.vec120.common;
 
-import com.foursoft.vecmodel.vec120.VecAbstractLocalizedString;
-import com.foursoft.vecmodel.vec120.VecLanguageCode;
-import com.foursoft.vecmodel.vec120.VecLocalizedTypedString;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
 @FunctionalInterface
-public interface HasDescription {
+public interface HasDescription<T> {
     /**
      * This method returns a {@link List } of all description objects this element owns. The list is of type
-     * {@link VecAbstractLocalizedString }, because the descriptions can have a specific (technical) type and therefore
-     * are of class {@link VecLocalizedTypedString }.
+     * {@link HasLocalizedString }, because the descriptions can have a specific (technical) type and therefore
+     * are of class {@link HasLocalizedTypeString }.
      *
-     * @return {@link List } of {@link VecAbstractLocalizedString } including all description objects.
+     * @return {@link List } of {@link HasLocalizedString } including all description objects.
      */
-    List<? extends VecAbstractLocalizedString> getDescriptions();
+    <R extends HasLocalizedString> List<R> getDescriptions();
 
     /**
      * This method returns a {@link List } of all typed descriptions of this element, filtered by the given
      * type as {@link String }.
      *
-     * @return {@link List } of {@link VecLocalizedTypedString } including all description objects of the given type.
+     * @return {@link List } of {@link HasLocalizedTypeString } including all description objects of the given type.
      */
-    default List<VecLocalizedTypedString> getDescriptions(String type){
+    default List<HasLocalizedTypeString> getDescriptions(final String type) {
         return getDescriptions().stream()
-                .filter(VecLocalizedTypedString.class::isInstance)
-                .map(VecLocalizedTypedString.class::cast)
+                .filter(HasLocalizedTypeString.class::isInstance)
+                .map(HasLocalizedTypeString.class::cast)
                 .filter(typedString -> type.equals(typedString.getType()))
                 .collect(Collectors.toList());
     }
 
     /**
      * This method returns a {@link List } of all description elements of this element, filtered by the
-     * given {@link VecLanguageCode }.
+     * given  VecLanguageCode.
      *
-     * @return {@link List } of {@link VecAbstractLocalizedString } including all description objects with the
-     * given {@link VecLanguageCode }.
+     * @return {@link List } of {@link T } including all description objects with the
+     * given VecLanguageCode.
      */
-    default List<? extends VecAbstractLocalizedString> getDescriptions(VecLanguageCode languageCode){
+    default List<HasLocalizedString> getDescriptions(final T languageCode) {
         return getDescriptions().stream()
                 .filter(typedString -> typedString.getLanguageCode() == languageCode)
                 .collect(Collectors.toList());
@@ -72,11 +68,11 @@ public interface HasDescription {
 
     /**
      * This method returns a {@link List } of all typed descriptions of this element, filtered by the given
-     * type as {@link String } and the given {@link VecLanguageCode }.
+     * type as {@link String } and the given VecLanguageCode.
      *
-     * @return {@link List } of {@link VecLocalizedTypedString } including all description objects of the given type.
+     * @return {@link List } of {@link HasLocalizedTypeString } including all description objects of the given type.
      */
-    default List<VecLocalizedTypedString> getDescriptions(VecLanguageCode languageCode, String type){
+    default List<HasLocalizedTypeString> getDescriptions(final T languageCode, final String type) {
         return getDescriptions(type).stream()
                 .filter(typedString -> typedString.getLanguageCode() == languageCode)
                 .collect(Collectors.toList());
